741c51e937d754bbe5ee7bf4e11f461328a301bd
amoghs09 <as8724@ic.ac.uk>
2025-06-03 18:10:12 +0100

    modified main function to handle input

--- src/emulate.c
+++ src/emulate.c
@@ -15,6 +15,38 @@ void initialise ( ARM_STATE *state ) {
   state->output = stdout;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
+  if (argc < 2) { 
+    fprintf(stderr, "Usage: ./emulate <binary_file> [output_file]\n");
+    return EXIT_FAILURE; 
+  }
+
+  const char *binary_filename = argv[1];
+  const char *output_filename = (argc > 2) ? argv[2] : NULL;
+  //optional output file - used when printing final state
+
+  ARM_STATE state;
+  initialise(&state);
+
+  if (state == NULL) {
+    return EXIT_FAILURE; //some sort of initialisation failure
+  }
+
+  //edit output based on optional file
+  if (output_filename != NULL) {
+    FILE *output_file = fopen(output_filename, "w");
+    if (output_file == NULL) {
+      fprintf(stderr, "Failed to open output file\n");
+      return EXIT_FAILURE; 
+    } 
+    state.output = output_file;
+  } 
+
+  //read file into memory
+
+  //fetch, decode, execute cycle
+
+  //print final state
+
   return EXIT_SUCCESS;
 }
