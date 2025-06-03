f27eceea9ea61ce893ea1fd3af98e5a9f803d84f
Ifechukwude Ndozi <idn24@oak05.doc.ic.ac.uk>
2025-06-03 16:16:48 +0100

    Emulator state and initialise function added

--- src/emulate.c
+++ src/emulate.c
@@ -1,4 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "emulator_state.h"
+
+void initialise ( ARM_STATE *state ) {
+  memset(state->memory, 0, MEM_SIZE);
+  memset(state->registers, 0, sizeof(state->registers));
+  state->pc = 0x0;
+  state->pstate.N = false;
+  state->pstate.Z = true;
+  state->pstate.C = false;
+  state->pstate.V = false;
+  state->halt_flag = false;
+  state->output = stdout;
+}
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
---
+++ src/emulator_state.h
@@ -0,0 +1,30 @@
+// header file for the state of the ARMV8 Machine
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Define constants for state
+#define MEM_SIZE 2097152 // 2 * 2^20
+#define NUM_GPR 31
+#define HALT_INSTRUCTION 0x8a000000
+
+// Struct for PSTATE register
+typedef struct {
+    bool N; // Negative
+    bool Z; // Zero
+    bool C; // Carry
+    bool V; // Overflow
+} PSTATE_Flags;
+
+//Struct for ARMV8 state
+typedef struct {
+    uint8_t memory[MEM_SIZE];
+    uint64_t registers[NUM_GPR];
+    uint64_t pc;
+    PSTATE_Flags pstate;
+    bool halt_flag;
+    FILE *output;
+} ARM_STATE;
+
