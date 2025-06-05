742f11cb30611d01d8d0402a44059e3701b0dd74
Rohan Desai <rd1424@vm-shell1.doc.ic.ac.uk>
2025-06-05 14:28:27 +0100

    suggested change to FDE cycle

--- src/emulate.c
+++ src/emulate.c
@@ -148,6 +148,7 @@ int main(int argc, char *argv[]) {
     //decode
     decode(&state);
     //execute
+    // rohan d comment: i think section 1.2 on spec suggests incrementing PC at the end of everything
   }
 
   //print final state
