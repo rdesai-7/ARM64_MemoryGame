a9632502972a5dcd707b830bed9863927cbc92ef
Amogh Shetty <as8724@oak20.doc.ic.ac.uk>
2025-06-06 02:36:04 +0100

    some changes

--- src/instr_execute.h
+++ src/instr_execute.h
@@ -7,8 +7,8 @@
 typedef void (*func_execute) ( ARM_STATE *state ) ;
 
 //Helper functions
-bool get_msb(uint64_t value, bool is_64bit)
-void update_pstate_arith( ARM_STATE *state, uint64_t operand1, uint64_t operand2, uint64_t result, bool is_sub, bool is_64_bit )
+bool get_msb(uint64_t value, bool is_64bit);
+void update_pstate_arith( ARM_STATE *state, uint64_t operand1, uint64_t operand2, uint64_t result, bool is_sub, bool is_64_bit );
 
 //Execute functions
 extern void execute(ARM_STATE *state);
