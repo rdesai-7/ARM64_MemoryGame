f14c77e1677d0d325759c614b874455f9ffad4c2
Rohan Desai <rd1424@vm-shell1.doc.ic.ac.uk>
2025-06-11 03:17:48 +0100

    Made basic introductory code for handling assembler input [rd1424]

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+  if (argc != 3) { 
+    fprintf(stderr, "Usage: ./assemble <file_in> <file_out> \n");
+    return EXIT_FAILURE; 
+  }
+
+  const char *input_filename = argv[1];
+  const char *output__filename = argv[2];
+
   return EXIT_SUCCESS;
 }
