3d62d140609871d8666612eab0edd960a70248b9
Ifechukwude Ndozi <idn24@ic.ac.uk>
2025-06-12 23:09:41 +0100

    First iteration of the first pass of the assembler idn24, rd1424

---
+++ src/pass_one.c
@@ -0,0 +1,101 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <ctype.h>
+#include "symbol_table.h"
+#include "pass_one.h"
+
+#define LINE_BUFFER 256
+
+
+char* trim_whitespace(char *s) {
+
+    while( isspace(*s) ) s++;
+    if(*s == '\0') return s;
+
+    char *end = s + strlen(s) - 1;
+    // Removes whitespace from the end
+    while ( (end > s && isspace(*end)) ) end--;
+    *(end + 1) = '\0';
+    return s;
+}
+
+bool is_line_empty(const char *line) {
+    if (line == NULL || *line == '\0') return true;
+    return false;
+}
+
+bool is_label(const char *line, char *label_name_out) {
+    // Trim whitespace should already be called
+
+    // No need to check first character is valid label character  
+    size_t len = strlen(line);
+    if (len - 1 >= MAX_LABEL_LENGTH) {
+        fprintf(stderr, "Label too long to fit");
+        return false;
+    }
+
+    // Check last character is colon
+    // Label_name_out will have enough space since it will be initialised with MAX_LABEL_LENGTH
+    if( line[len - 1] == ':' ) {
+        strncpy( label_name_out, line, len -1);
+        label_name_out[len - 1] = '\0';
+        return true;
+    }
+
+    return false;
+}
+
+bool run_pass_one( const char *filename, SymbolTable_t st, uint32_t *program_size ) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+      fprintf(stderr, "Error Opening File");
+      return false;
+    }
+
+    char line_buffer[LINE_BUFFER];
+    uint32_t curr_address = 0x0;
+
+    while( fgets(line_buffer, sizeof(line_buffer), file) != NULL ) {
+        char *line = trim_whitespace(line_buffer);
+
+        if( is_line_empty(line) ) {
+            continue;
+        }
+
+        char label_name[MAX_LABEL_LENGTH];
+        
+        // Check if the line is a label and stores it in label_name
+        if( is_label( line, label_name ) ) {
+            // Add label and address to symbol table should make function return a bool for error checking
+            addSymbolEntry( st, label_name, curr_address );
+            
+        } else {
+            // If its not a label or empty line then it will be an instruction or a directive and these are all 4 bytes so increment by 4
+            curr_address += 4;
+        }
+
+    }
+    
+    // Stnadard error checking
+    if (feof(file)) {
+        fprintf(stdout, "Reached end of file.\n");
+        fclose(file);
+        return false;
+    } else if (ferror(file)) {
+        fprintf(stderr, "Error reading from file");
+        fclose(file);
+        return false;
+    }
+
+    fclose(file);
+    asser(program_size != NULL);
+    // Store the size of the program might be useful
+    *program_size = curr_address;
+    return true;
+}
+
+
