c5f96570af93c055f89ac457ddd212ac4170a485
Rohan Desai <rd1424@vm-shell1.doc.ic.ac.uk>
2025-06-14 18:16:41 +0100

    Began developing int directive functionality for pass two [rd1424]

--- src/assemble.c
+++ src/assemble.c
@@ -3,32 +3,6 @@
 #include <stdint.h>
 #include <symbol_table_h>
 
-/*
-void secondPass( const char *filename, SymbolTable_t st); {
-  FILE *file = fopen(filename, "r");
-
-  while (true) {
-    char buffer[MAX_LABEL_LENGTH];
-    fscanf(file, "%[^\n]", buffer);
-    if (feof(file)) { //stop reading at end of file
-      break;
-    }
-
-    int strLength = strlen(buffer) + 1;
-    if (buffer[strLength - 2] != ':') { //skipping past for labels
-        //deal with .int directives
-        //split into tokens
-        //parse each instruction
-        //call a parse function that calls one of 4 separate parse functions depending on instruction type
-    }
-  }
-}
-*/
-
-//instructions need to be stored somewhere
-//make an assembler state type that stores the symbol table, the binary instructions (in a memory array format), and the current address
-
-
 int main(int argc, char **argv) {
   if (argc != 3) { 
     fprintf(stderr, "Usage: ./assemble <file_in> <file_out> \n");
---
+++ src/pass_two.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "symbol_table.h"
+#include "pass_one.h"
+
+bool is_directive(char *s) {
+    return strncmp(s, ".int", 4) == 0;
+}
+
+uint32_t parse_directive(char *line) {
+    // PRE: is_directive(line) returns true
+    int value;
+    // sscanf parses hex and decimal values using %i
+    assert(sscanf(line, ".int %i", &value) == 1);
+    return (uint32_t) value;
+}
+
+
+void run_pass_two( const char *filename, SymbolTable_t st) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error Opening File");
+        exit(1);
+    }
+
+    char line_buffer[LINE_BUFFER];
+    uint32_t curr_address = 0x0;
+
+    while( fgets(line_buffer, sizeof(line_buffer), file) != NULL ) {
+        char *line = trim_whitespace(line_buffer);
+        char label_name[MAX_LABEL_LENGTH];
+
+        if( !is_line_empty(line) && !is_label(line,label_name)) {
+
+            //deal with .int directives
+            if (is_directive(line)) {
+                uint32_t N = parse_directive(line);
+                // idk what to do here...
+                // store N at the curr_address i think. how do you write that in binary though?
+            }
+
+            //split into tokens
+
+            //parse each instruction
+
+            //call a parse function that calls one of 4 separate parse functions depending on instruction type
+
+
+            curr_address += ADDR_INCREMENT;
+        }
+    }
+
+    // Standard error checking
+    if (ferror(file)) {
+        fprintf(stderr, "Error reading from file");
+        fclose(file);
+        exit(1);
+    }
+
+    fclose(file);
+}
+
+
+//instructions need to be stored somewhere
+//make an assembler state type that stores the symbol table, the binary instructions (in a memory array format), and the current address
+
